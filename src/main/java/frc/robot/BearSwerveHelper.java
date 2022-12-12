d8bc9e430103c2339131f5a1894bf94c6fd3833d
Evan <cumberlandrobotics@gmail.com>
2022-12-11 21:59:09 -0500

    CHanged Motors to Neos, Using NavX for Gyro

--- src/main/java/frc/robot/BearSwerveHelper.java
+++ src/main/java/frc/robot/BearSwerveHelper.java
@@ -10,12 +10,13 @@ import frc.robot.Constants.DRIVE;
 import frc.robot.Constants.ROBOT;
 import frc.swervelib.Gyroscope;
 import frc.swervelib.GyroscopeHelper;
-import frc.swervelib.Mk4SwerveModuleHelper;
+import frc.swervelib.Mk4iSwerveModuleHelper;
 import frc.swervelib.SwerveConstants;
 import frc.swervelib.SwerveModule;
 import frc.swervelib.SwerveSubsystem;
 import frc.swervelib.SwerveDrivetrainModel;
 import frc.wpiClasses.QuadSwerveSim;
+
 
 public class BearSwerveHelper {
     public static SwerveDrivetrainModel createBearSwerve() {
@@ -23,33 +24,14 @@ public class BearSwerveHelper {
         ShuffleboardTab tab = Shuffleboard.getTab("Drivetrain");
         ArrayList<SwerveModule> modules = new ArrayList<SwerveModule>(QuadSwerveSim.NUM_MODULES);
 
-        // There are 4 methods you can call to create your swerve modules.
-        // The method you use depends on what motors you are using.
-        //
-        // Mk3SwerveModuleHelper.createFalcon500(...)
-        //   Your module has two Falcon 500s on it. One for steering and one for driving.
-        //
-        // Mk3SwerveModuleHelper.createNeo(...)
-        //   Your module has two NEOs on it. One for steering and one for driving.
-        //
-        // Mk3SwerveModuleHelper.createFalcon500Neo(...)
-        //   Your module has a Falcon 500 and a NEO on it. The Falcon 500 is for driving and the NEO is for steering.
-        //
-        // Mk3SwerveModuleHelper.createNeoFalcon500(...)
-        //   Your module has a NEO and a Falcon 500 on it. The NEO is for driving and the Falcon 500 is for steering.
-        //
-        // Similar helpers also exist for Mk4 modules using the Mk4SwerveModuleHelper class.
-    
-        // By default we will use Falcon 500s in standard configuration. But if you use a different configuration or motors
-        // you MUST change it. If you do not, your code will crash on startup.
-        // FIXME Setup motor configuration
-        SwerveModule m_frontLeftModule = Mk4SwerveModuleHelper.createFalcon500(
+        // Setup motor configuration - CreateNeo is two Neos, there are options for falcons
+        SwerveModule m_frontLeftModule = Mk4iSwerveModuleHelper.createNeo(
                 // This parameter is optional, but will allow you to see the current state of the module on the dashboard.
                 tab.getLayout("Front Left Module", BuiltInLayouts.kList)
                         .withSize(2, 4)
                         .withPosition(0, 0),
                 // This can either be STANDARD or FAST depending on your gear configuration
-                Mk4SwerveModuleHelper.GearRatio.L1,
+                Mk4iSwerveModuleHelper.GearRatio.L1,
                 // This is the ID of the drive motor
                 DRIVE.FRONT_LEFT_MODULE_DRIVE_MOTOR,
                 // This is the ID of the steer motor
@@ -61,40 +43,40 @@ public class BearSwerveHelper {
         );
     
         // We will do the same for the other modules
-        SwerveModule m_frontRightModule = Mk4SwerveModuleHelper.createFalcon500(
+        SwerveModule m_frontRightModule = Mk4iSwerveModuleHelper.createNeo(
                 tab.getLayout("Front Right Module", BuiltInLayouts.kList)
                         .withSize(2, 4)
                         .withPosition(2, 0),
-                Mk4SwerveModuleHelper.GearRatio.L1,
+                Mk4iSwerveModuleHelper.GearRatio.L1,
                 DRIVE.FRONT_RIGHT_MODULE_DRIVE_MOTOR,
                 DRIVE.FRONT_RIGHT_MODULE_STEER_MOTOR,
                 DRIVE.FRONT_RIGHT_MODULE_STEER_ENCODER,
                 DRIVE.FRONT_RIGHT_MODULE_STEER_OFFSET, "FR"
         );
     
-        SwerveModule m_backLeftModule = Mk4SwerveModuleHelper.createFalcon500(
+        SwerveModule m_backLeftModule = Mk4iSwerveModuleHelper.createNeo(
                 tab.getLayout("Back Left Module", BuiltInLayouts.kList)
                         .withSize(2, 4)
                         .withPosition(4, 0),
-                Mk4SwerveModuleHelper.GearRatio.L1,
+                Mk4iSwerveModuleHelper.GearRatio.L1,
                 DRIVE.BACK_LEFT_MODULE_DRIVE_MOTOR,
                 DRIVE.BACK_LEFT_MODULE_STEER_MOTOR,
                 DRIVE.BACK_LEFT_MODULE_STEER_ENCODER,
                 DRIVE.BACK_LEFT_MODULE_STEER_OFFSET, "BL"
         );
     
-        SwerveModule m_backRightModule = Mk4SwerveModuleHelper.createFalcon500(
+        SwerveModule m_backRightModule = Mk4iSwerveModuleHelper.createNeo(
                 tab.getLayout("Back Right Module", BuiltInLayouts.kList)
                         .withSize(2, 4)
                         .withPosition(6, 0),
-                Mk4SwerveModuleHelper.GearRatio.L1,
+                Mk4iSwerveModuleHelper.GearRatio.L1,
                 DRIVE.BACK_RIGHT_MODULE_DRIVE_MOTOR,
                 DRIVE.BACK_RIGHT_MODULE_STEER_MOTOR,
                 DRIVE.BACK_RIGHT_MODULE_STEER_ENCODER,
                 DRIVE.BACK_RIGHT_MODULE_STEER_OFFSET, "BR"
         );
-    
-        Gyroscope gyro = GyroscopeHelper.createPigeonCAN(DRIVE.PIGEON_ID);
+    //you can use the pigeon gyro here by changing GyroHelper.createPigeon and its CANBUS address 
+        Gyroscope gyro = GyroscopeHelper.createnavXMXP();
     
         modules.add(m_frontLeftModule);
         modules.add(m_frontRightModule);
--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -49,13 +49,13 @@ public final class Constants {
          *
          * Should be measured from center to center.
          */
-        public static final double TRACKWIDTH_METERS = 1.0; // FIXME Measure and set trackwidth
+        public static final double TRACKWIDTH_METERS = .560; //  Measure and set trackwidth
         /**
          * The front-to-back distance between the drivetrain wheels.
          *
          * Should be measured from center to center.
          */
-        public static final double WHEELBASE_METERS = 1.0; // FIXME Measure and set wheelbase
+        public static final double WHEELBASE_METERS = .545; // FIXME Measure and set wheelbase
 
         public static final SwerveDriveKinematics KINEMATICS = new SwerveDriveKinematics(
                 new Translation2d(DRIVE.TRACKWIDTH_METERS / 2.0, DRIVE.WHEELBASE_METERS / 2.0),
@@ -67,27 +67,35 @@ public final class Constants {
         public static final double WHEEL_DIAMETER_METERS = 0.10033; // .10033 = ~4 inches
         public static final double WHEEL_CIRCUMFERENCE_METERS = WHEEL_DIAMETER_METERS * Math.PI;
 
-        public static final int PIGEON_ID = 0; // FIXME Set Pigeon ID
+        public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR = 1; // FIXME Set front left module drive motor ID
+        public static final int FRONT_LEFT_MODULE_STEER_MOTOR = 2; // FIXME Set front left module steer motor ID
+        public static final int FRONT_LEFT_MODULE_STEER_ENCODER = 3; // FIXME Set front left steer encoder ID
+        public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(74); // FIXME Measure and set front left steer offset
 
-        public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR = 0; // FIXME Set front left module drive motor ID
-        public static final int FRONT_LEFT_MODULE_STEER_MOTOR = 1; // FIXME Set front left module steer motor ID
-        public static final int FRONT_LEFT_MODULE_STEER_ENCODER = 0; // FIXME Set front left steer encoder ID
-        public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(0.0); // FIXME Measure and set front left steer offset
+        public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR = 4; // FIXME Set front right drive motor ID
+        public static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 5; // FIXME Set front right steer motor ID
+        public static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 7; // FIXME Set front right steer encoder ID
+        public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(339.8); // FIXME Measure and set front right steer offset
 
-        public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR = 2; // FIXME Set front right drive motor ID
-        public static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 3; // FIXME Set front right steer motor ID
-        public static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 1; // FIXME Set front right steer encoder ID
-        public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(0.0); // FIXME Measure and set front right steer offset
+        public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 11; // FIXME Set back left drive motor ID
+        public static final int BACK_LEFT_MODULE_STEER_MOTOR = 12; // FIXME Set back left steer motor ID
+        public static final int BACK_LEFT_MODULE_STEER_ENCODER = 13; // FIXME Set back left steer encoder ID
+        public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(299.9); // FIXME Measure and set back left steer offset
 
-        public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 4; // FIXME Set back left drive motor ID
-        public static final int BACK_LEFT_MODULE_STEER_MOTOR = 5; // FIXME Set back left steer motor ID
-        public static final int BACK_LEFT_MODULE_STEER_ENCODER = 2; // FIXME Set back left steer encoder ID
-        public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(0.0); // FIXME Measure and set back left steer offset
+        public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 8; // FIXME Set back right drive motor ID
+        public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 9; // FIXME Set back right steer motor ID
+        public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 10; // FIXME Set back right steer encoder ID
+        public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(254.8); // FIXME Measure and set back right steer offset        
 
-        public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 6; // FIXME Set back right drive motor ID
-        public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 7; // FIXME Set back right steer motor ID
-        public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 3; // FIXME Set back right steer encoder ID
-        public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(0.0); // FIXME Measure and set back right steer offset        
+
+        public static final int AIR = 14; //Air system control module
+        public static final int POWER = 6; //Power Distribution Board
+    
+        public static final int INTAKE = 15;
+        public static final int CONVEYOR = 16;
+        public static final int RIGHTSHOOTER = 17;
+        public static final int LEFTSHOOTER = 18;
+
 
         // Drivetrain Performance Mechanical limits
         static public final double MAX_FWD_REV_SPEED_MPS = Units.feetToMeters(12.0);
@@ -100,8 +108,10 @@ public final class Constants {
         /////////////////////////////////////////////////////////////////////////////////////////////////////////////
         // SENSOR CONSTANTS
         // Sensor-related constants - pulled from datasheets for the sensors and gearboxes
-        static public final int ENC_PULSE_PER_REV = 2048; // TalonFX integrated sensor
-        static public final int WHEEL_ENC_COUNTS_PER_WHEEL_REV = ENC_PULSE_PER_REV;  //Assume 1-1 gearing for now
+        static public final int ENC_PULSE_PER_REV = 42; // Neo integrated sensor
+        static public final int WHEEL_ENC_COUNTS_PER_WHEEL_REV = 342;
+        //L1 gear ratio is (14.0 / 50.0) * (25.0 / 19.0) * (15.0 / 45.0) or 0.1228070175438596.
+        //1 wheel rev would mean motor needs to spin 8.14 times, 8.14 times 42 is 342? 
         static public final int STEER_ENC_COUNTS_PER_MODULE_REV = 4096; // CANCoder
         static public final double WHEEL_ENC_WHEEL_REVS_PER_COUNT  = 1.0/((double)(WHEEL_ENC_COUNTS_PER_WHEEL_REV));
         static public final double steer_ENC_MODULE_REVS_PER_COUNT = 1.0/((double)(STEER_ENC_COUNTS_PER_MODULE_REV));
@@ -116,6 +126,8 @@ public final class Constants {
         public static final double TRAJECTORYXkP = 1;
         public static final double TRAJECTORYYkP = 1;
         public static final double THETACONTROLLERkP = 1;
+
+        //Previous version of software had 30, 30 and 1 above
 
         // Constraint for the motion profilied robot angle controller
         public static final TrapezoidProfile.Constraints THETACONTROLLERCONSTRAINTS =
